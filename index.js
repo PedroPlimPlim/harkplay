6e7be0dbdca1fdd211a6098425e843a062727f28
PedroPlimPlim <47133270+PedroPlimPlim@users.noreply.github.com>
2019-01-29 02:10:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var tmi = require('tmi.js');
+var options = 
+{
+	options: 
+	{
+		debug: true
+	},
+	connection: 
+	{
+		cluster: "aws",
+		reconnect: true
+	},
+	identity: 
+	{
+		username: "PedroPlimPlim2",
+		password: "oauth:j0doj0s6heces8k62154wlhqkjd5mf",  
+	},
+	channels: ['#johnpittertv', '#gaules',] //ADICIONA OS CANAIS AQ COM '#NOME'
+};
+
+var client = new tmi.client(options);
+
+	client.connect();
+	client.on("connected", function (address, port)
+	{
+		console.log("address: " + address + " port: " + port);
+		client.action("999mateus", "Hello 999mateus!")
+	});
